86c974939f1dca4ecf27b303dfdede1e8e9e73c4
Dario Quintero Dominguez <dq16394@bristol.ac.uk>
2020-01-04 21:08:19 +0000

    Initial moments of signal processing

--- compassControlParameters.m
+++ compassControlParameters.m
@@ -18,9 +18,9 @@ else
 end
 
 %% Frequency Analysis
-secondOrderSystem = tf(compassTransferFunctionNumerator, compassTransferFunctionDenominator)
+secondOrderSystem = tf(compassTransferFunctionNumerator, compassTransferFunctionDenominator);
 [magnitudeSecondOrder, phaseSecondOrder, frequenciesSecondOrder] = bode(secondOrderSystem);
-maximumSlopeSecondOrder3dBLimit = [0.15, 1];
+maximumSlopeSecondOrder3dBLimit = [0.15, 0.2];
 % figure % Figure nly to check
 % bode(secondOrderSystem);
 
@@ -34,38 +34,40 @@ end
 rawMagnitudeDBSecondOrder = [transpose(frequenciesSecondOrder); iteratorMagnitudeDBSecondOrder];
 
 % All Filtered dB Magnitude points
+j = 1;
 filteredMagnitudeDBSecondOrder(size(rawMagnitudeDBSecondOrder)) = 0;
 filteredPhaseDBSecondOrder(size(rawMagnitudeDBSecondOrder)) = 0;
 badFilteredMagnitudeDBSecondOrder(size(rawMagnitudeDBSecondOrder)) = 0;
 badFilteredPhaseDBSecondOrder(size(rawMagnitudeDBSecondOrder)) = 0;
 
+
+for magnitudeValue = rawMagnitudeDBSecondOrder(2,:)
+    if (magnitudeValue >= -3) && (magnitudeValue <= 3)
+        if j > 1
+            if abs(rawMagnitudeDBSecondOrder(1,j) - rawMagnitudeDBSecondOrder(1,j-1)) <= maximumSlopeSecondOrder3dBLimit(1)
+                filteredMagnitudeDBSecondOrder(1,j) = rawMagnitudeDBSecondOrder(1,j);
+                filteredMagnitudeDBSecondOrder(2,j) = rawMagnitudeDBSecondOrder(2,j);
+                filteredPhaseDBSecondOrder(1,j) = rawMagnitudeDBSecondOrder(1,j);
+                filteredPhaseDBSecondOrder(2,j) = phaseSecondOrder(j);
+            else
+                badFilteredMagnitudeDBSecondOrder(1,j) = rawMagnitudeDBSecondOrder(1,j);
+                badFilteredMagnitudeDBSecondOrder(2,j) = rawMagnitudeDBSecondOrder(2,j);
+                badFilteredPhaseDBSecondOrder(1,j) = rawMagnitudeDBSecondOrder(1,j);
+                badFilteredPhaseDBSecondOrder(2,j) = phaseSecondOrder(j);
+            end
+        else
+            filteredMagnitudeDBSecondOrder(1,j) = rawMagnitudeDBSecondOrder(1,j);
+            filteredMagnitudeDBSecondOrder(2,j) = rawMagnitudeDBSecondOrder(2,j);
+            filteredPhaseDBSecondOrder(1,j) = rawMagnitudeDBSecondOrder(1,j);
+            filteredPhaseDBSecondOrder(2,j) = phaseSecondOrder(j);
+        end
+    end 
+    j = j + 1;
+end
+
 k = 1; % Slope iterator
 for slopeSecondOrder3dBLimit = maximumSlopeSecondOrder3dBLimit
-    j = 1;
-    for magnitudeValue = rawMagnitudeDBSecondOrder(2,:)
-        if (magnitudeValue >= -3) && (magnitudeValue <= 3)
-            if j > 1
-                if abs(rawMagnitudeDBSecondOrder(1,j) - rawMagnitudeDBSecondOrder(1,j-1)) <= maximumSlopeSecondOrder3dBLimit(k)
-                    filteredMagnitudeDBSecondOrder(1,j,k) = rawMagnitudeDBSecondOrder(1,j);
-                    filteredMagnitudeDBSecondOrder(2,j,k) = rawMagnitudeDBSecondOrder(2,j);
-                    filteredPhaseDBSecondOrder(1,j,k) = rawMagnitudeDBSecondOrder(1,j);
-                    filteredPhaseDBSecondOrder(2,j,k) = phaseSecondOrder(j);
-                else
-                    badFilteredMagnitudeDBSecondOrder(1,j,k) = rawMagnitudeDBSecondOrder(1,j);
-                    badFilteredMagnitudeDBSecondOrder(2,j,k) = rawMagnitudeDBSecondOrder(2,j);
-                    badFilteredPhaseDBSecondOrder(1,j,k) = rawMagnitudeDBSecondOrder(1,j);
-                    badFilteredPhaseDBSecondOrder(2,j,k) = phaseSecondOrder(j);
-                end
-            else
-                filteredMagnitudeDBSecondOrder(1,j,k) = rawMagnitudeDBSecondOrder(1,j);
-                filteredMagnitudeDBSecondOrder(2,j,k) = rawMagnitudeDBSecondOrder(2,j);
-                filteredPhaseDBSecondOrder(1,j,k) = rawMagnitudeDBSecondOrder(1,j);
-                filteredPhaseDBSecondOrder(2,j,k) = phaseSecondOrder(j);
-            end
-
-        end 
-        j = j + 1;
-    end
+    
     k = k + 1;
 end
 
@@ -82,8 +84,8 @@ maxMagnitudeFilteredMagnitudeDBSecondOrder = ...
 figure
 hold on
 plot(frequenciesSecondOrder, iteratorMagnitudeDBSecondOrder);
-% yline(-3);
-% yline(3);
+yline(-3);
+yline(3);
 plot(filteredMagnitudeDBSecondOrder(1,:), ...
     filteredMagnitudeDBSecondOrder(2,:), '.b')
 plot(badFilteredMagnitudeDBSecondOrder(1,:), ...
@@ -106,7 +108,7 @@ set(gca, 'XScale', 'log')
 hold off
 
 disp("The 3dB cutoff maximum frequency is limited at a maximum slope of "...
-    + maximumSlopeSecondOrder3dBLimit + ". " + newline + ...
+    + maximumSlopeSecondOrder3dBLimit(1) + ". " + newline + ...
     "The minimum and maximum safe operational frequency of the sensor is: " + newline + ...
     + maxFrequencyFilteredMagnitudeDBSecondOrder + " rad/s with a "...
     + "maximum magnitude of " + maxMagnitudeFilteredMagnitudeDBSecondOrder + " dB");
--- signalPostProcessing.m
+++ signalPostProcessing.m
@@ -7,3 +7,20 @@ simulation = sim('fullSystem','SimulationMode','normal');
 % Get results
 simulationData = simulation.get('simulationData');
 assignin('base','simulationData',simulationData);
+
+compassSystem = simulationData.signals.values(:, 1);
+compassFilter = simulationData.signals.values(:, 2);
+fullSystem = simulationData.signals.values(:, 3);
+gyroSystem = simulationData.signals.values(:, 4);
+gyroFilter = simulationData.signals.values(:, 5);
+input = simulationData.signals.values(:, 6);
+time = simulationData.time;
+
+meanInput = mean(input);
+meanCompassSystem = mean(compassSystem);
+meanCompassFilter = mean(compassFilter);
+meanFullSystem = mean(fullSystem);
+meanGyroSystem = mean(gyroSystem);
+meanGyroFilter = mean(gyroFilter);
+
+
