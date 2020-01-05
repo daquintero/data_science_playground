900699572592b6bb6f02a4acd52e550aeaba3452
Dario Quintero <darioaquintero@gmail.com>
2020-01-05 09:48:19 +0000

    Signal processing code added

--- signalPostProcessing.m
+++ signalPostProcessing.m
@@ -8,6 +8,7 @@ simulation = sim('fullSystem','SimulationMode','normal');
 simulationData = simulation.get('simulationData');
 assignin('base','simulationData',simulationData);
 
+allData = simulationData.signals.values(:, :);
 compassSystem = simulationData.signals.values(:, 1);
 compassFilter = simulationData.signals.values(:, 2);
 fullSystem = simulationData.signals.values(:, 3);
@@ -16,11 +17,31 @@ gyroFilter = simulationData.signals.values(:, 5);
 input = simulationData.signals.values(:, 6);
 time = simulationData.time;
 
-meanInput = mean(input);
-meanCompassSystem = mean(compassSystem);
-meanCompassFilter = mean(compassFilter);
-meanFullSystem = mean(fullSystem);
-meanGyroSystem = mean(gyroSystem);
-meanGyroFilter = mean(gyroFilter);
+% meanInput = mean(input);
+% meanCompassSystem = mean(compassSystem);
+% meanCompassFilter = mean(compassFilter);
+% meanFullSystem = mean(fullSystem);
+% meanGyroSystem = mean(gyroSystem);
+% meanGyroFilter = mean(gyroFilter);
 
+%% Signals Processing
+meanSignals = mean(allData(:, :));
+standardDeviationSignals = std(allData(:, :));
+varianceSignals = var(allData(:, :));
+maxSignals = max(allData(:, :));
+minSignals = min(allData(:, :));
+kurtosisSignals = kurtosis(allData(:, :));
+firstMomentSignals = moment(allData(:, :), 1);
+secondMomentSignals = moment(allData(:, :), 2);
+thirdMomentSignals = moment(allData(:, :), 3);
+fourthMomentSignals = moment(allData(:, :), 4);
+skewnessSignals = skewness(allData(:, :));
+
+%% Spectral Kurtuosis
+i = 1;
+for signalsAmount = 1:6
+    figure
+    pkurtosis(allData(:, i))
+    i = i + 1;
+end
 
