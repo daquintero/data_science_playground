22fd4b0704d7359ee1cebc74f0b8a2ecce1a4c45
Dario Quintero Dominguez <dq16394@bristol.ac.uk>
2020-01-13 14:53:39 +0000

    Stable

--- HeadingSignals.m
+++ HeadingSignals.m
@@ -72,33 +72,42 @@ classdef HeadingSignals < matlab.System
             
             % Time limits
             step1Time = 3;
-            transition12EndTime = step1Time + 5;
-            step2Time = transition12EndTime + 10;
-            transition23EndTime = step2Time + 2;
-            step3Time = transition23EndTime + 10;
-            
+            transition12EndTime = step1Time + 5; % 8
+            step2Time = transition12EndTime + 5; % 13
+            transition23EndTime = step2Time + 2; % 15
+            step3Time = transition23EndTime + 5; % 20
+            transition34EndTime = step3Time + 5; % 25
+            step4Time = transition34EndTime + 5; % 30
             
             % State at those specific times
             step1State = 1;
             step2State = maximumTurnRate/2;
             transitionState12 = (time - step1Time) * (step2State - step1State) / (transition12EndTime - step1Time);
-            step3State = -maximumTurnRate;
-            transitionState23 = 3 * (time - step2Time) * (step3State - step2State) / (transition23EndTime - step2Time);
+            step3State = -maximumTurnRate/2;
+            transitionState23 = (time - step2Time -1) * (step3State - step2State) / (transition23EndTime - step2Time)
+            step4State = -maximumTurnRate/4;
+            transitionState34 = (time - step3Time) * (step4State - step3State) / (transition34EndTime - step3Time);
             
-            if time < step1Time
+            if time <= step1Time
                 trueHeading = step1State;
-            elseif (time >= step1Time) && (time < transition12EndTime)
+            elseif (time > step1Time) && (time <= transition12EndTime)
                 trueHeading = transitionState12;
-            elseif (time >= transition12EndTime) && (time < step2Time)
+            elseif (time > transition12EndTime) && (time <= step2Time)
                 trueHeading = step2State;
-            elseif (time >= step2Time) && (time < transition23EndTime)
-                trueHeading = transitionState23;
-            elseif (time >= transition23EndTime) && (time < step3Time)
+            elseif (time > step2Time) && (time <= transition23EndTime)
+                trueHeading = transitionState23
+            elseif (time > transition23EndTime) && (time <= step3Time)
                 trueHeading = step3State;
+%             elseif (time >= step3Time) && (time < transition34EndTime)
+%                 trueHeading = transitionState34;
+%             elseif (time >= transition34EndTime) && (time < step4Time)
+%                 trueHeading = step4State;
+%             elseif (time >= step4Time)
+%                 trueHeading = step4State/2 * sin((time-step4Time)) - 50;
             else
-                trueHeading = 10;
+                trueHeading = step3State;
             end
-            trueHeading = trueHeading + random("normal", 0, maximumTurnRate * 0.005);
+            trueHeading = trueHeading + random("normal", 0, maximumTurnRate * 0.005)
         end
     end
 end
